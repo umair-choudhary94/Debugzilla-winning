270f6f9e2ceb182857d2747a991d2fd00bab706a
Umair Sabir <umairsabirjutt@gmail.com>
2025-01-03 03:15:08 +0500

    added database

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    project_name = models.CharField(max_length=255)
+    project_description = models.TextField()
+    link = models.URLField()
+    def __str__(self):
+        return self.project_name
+
