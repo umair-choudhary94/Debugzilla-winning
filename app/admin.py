ddfeb7256fd4bb76b30833c01abc135180d09f80
Masafy Kamran <umairsabirjutt@gmail.com>
2025-08-04 15:58:29 +0500

    fixed thumnail issue

--- app/admin.py
+++ app/admin.py
@@ -3,7 +3,7 @@ from django.utils.safestring import mark_safe
 from .models import *
 
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_datetime', 'thumbnail_preview', 'created_at')
+    list_display = ('title', 'author', 'publication_datetime', 'created_at')
     list_filter = ('publication_datetime', 'author')
     search_fields = ('title', 'description', 'author')
     prepopulated_fields = {'slug': ('title',)}  # Auto-fill slug from title
